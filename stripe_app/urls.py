7c8fd146020a0eef4504cf56ea638eb268af03c0
lev <kakalashvilillev@yandex.ru>
2022-09-12 23:27:39 +0300

    chore: items api endpoint. изменил способ выдачи

--- stripe_app/urls.py
+++ stripe_app/urls.py
@@ -1,10 +1,14 @@
 from django.urls import include, path
 from rest_framework import routers
-from stripe_app.views import ItemViewSet
+from rest_framework.urlpatterns import format_suffix_patterns
 
-router = routers.DefaultRouter()
-router.register("items", ItemViewSet)
+from stripe_app.views import ItemsListView
+#
+# router = routers.DefaultRouter()
+# router.register("items", ItemViewSet)
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('api/items/', ItemsListView.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- stripe_app/views.py
+++ stripe_app/views.py
@@ -4,8 +4,17 @@ from rest_framework import viewsets
 
 from stripe_app.serializers import ItemSerializer
 from stripe_app.models import Item
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
-class ItemViewSet(viewsets.ModelViewSet):
-    queryset = Item.objects.all().order_by("name")
-    serializer_class = ItemSerializer
+# class ItemViewSet(viewsets.ModelViewSet):
+#     queryset = Item.objects.all().order_by("name")
+#     serializer_class = ItemSerializer
+
+
+class ItemsListView(APIView):
+    def get(self, request, format=None):
+        items = Item.objects.all()
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data)
