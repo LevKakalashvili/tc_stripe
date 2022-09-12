b52b3d905f88f2a5eff80e4e3a6039489a7c39dd
Lev <kakalashvililev@yandex.ru>
2022-09-12 16:49:08 +0300

    init: models. модель item

--- stripe_app/models.py
+++ stripe_app/models.py
@@ -1,3 +1,9 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Идентификатор товара")
+    name = models.CharField(max_length=70, unique=True, help_text="Наименование товара")
+    description = models.CharField(max_length=20, help_text="Описание товара")
+    price = models.DecimalField(max_digits=5, decimal_places=2, help_text="Цена товара")
