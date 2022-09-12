58de8e3271f64be067a71897016391b3f0da7f81
lev <kakalashvilillev@yandex.ru>
2022-09-12 22:56:18 +0300

    refactor: убрал лишнее

--- stripe_app/views.py
+++ stripe_app/views.py
@@ -5,6 +5,7 @@ from rest_framework import viewsets
 from stripe_app.serializers import ItemSerializer
 from stripe_app.models import Item
 
+
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all().order_by("name")
     serializer_class = ItemSerializer
