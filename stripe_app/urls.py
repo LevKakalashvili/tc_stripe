957472fe623cc73a98e3b300ad44d6f50233ae40
Lev <kakalashvililev@yandex.ru>
2022-09-13 16:30:44 +0300

    feat: new endpoint. Добавил /buy/{id} и его обработку

--- stripe_app/urls.py
+++ stripe_app/urls.py
@@ -1,14 +1,16 @@
 from django.urls import include, path
+# from django.conf.urls
 from rest_framework import routers
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from stripe_app.views import ItemsListView
-#
-# router = routers.DefaultRouter()
-# router.register("items", ItemViewSet)
+from stripe_app.views import ItemsListView, ItemBuyView
+
+
+API_PATH = "api"
 
 urlpatterns = [
-    path('api/items/', ItemsListView.as_view()),
+    path(f"{API_PATH}/items/", ItemsListView.as_view()),
+    path(f"{API_PATH}/buy/<str:item_uuid>", ItemBuyView.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- stripe_app/views.py
+++ stripe_app/views.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.shortcuts import render
 
 from rest_framework import viewsets
@@ -7,11 +9,11 @@ from stripe_app.models import Item
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-
-# class ItemViewSet(viewsets.ModelViewSet):
-#     queryset = Item.objects.all().order_by("name")
-#     serializer_class = ItemSerializer
-
+class ItemBuyView(APIView):
+    def get(self, request, item_uuid: str, format=None):
+        items = Item.objects.all()
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data)
 
 class ItemsListView(APIView):
     def get(self, request, format=None):
